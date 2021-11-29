19b486d8e02f8162c913413ff91db5256655df1a
Mou7amed0 <outloohou@outlook.com>
2021-11-29 14:46:51 +0100

    fix some misspillings.

--- TP1_J2EE/src/main/java/database/UserDatabase.java
+++ TP1_J2EE/src/main/java/database/UserDatabase.java
@@ -20,6 +20,7 @@ public class UserDatabase {
 	private String dbpw = "";
 	private String dbdriver = "com.mysql.jdbc.Driver";
 	private DataSource dataSource;
+
 	public void loadDriver(String dbDriver) {
 		try {
 			Class.forName(dbDriver);
@@ -28,9 +29,10 @@ public class UserDatabase {
 			e.printStackTrace();
 		}
 	}
+
 	public Connection getConnection() {
 		loadDriver(dbdriver);
-		Connection conn=null;
+		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection(dburl, dbuname, dbpw);
 			System.out.println("You're connected to database");
@@ -41,91 +43,92 @@ public class UserDatabase {
 			return conn;
 		}
 	}
-	
-public String insert(User usr) {
-	Connection conn = getConnection();
-	String result = "Data entered successfully";
-	String sql = "INSERT INTO tp1.users VALUES(?,?,?,?,?,?,?)";
-	try {
-		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setString(1, usr.getFirstName());
-		ps.setString(2, usr.getLastName());
-		ps.setString(3, usr.getEmail());
-		ps.setString(4, usr.getMobile());
-		ps.setString(5, usr.getDateOfBirth());
-		ps.setString(6, usr.getUsername());
-		ps.setString(7, usr.getPassword());
-		ps.executeUpdate();
-		// close JDBC objects
-		conn.close();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		result = "Something went wrong !!";	
+
+	public String insert(User usr) {
+		Connection conn = getConnection();
+		String result = "Data entered successfully";
+		String sql = "INSERT INTO tp1.users VALUES(?,?,?,?,?,?,?)";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, usr.getFirstName());
+			ps.setString(2, usr.getLastName());
+			ps.setString(3, usr.getEmail());
+			ps.setString(4, usr.getMobile());
+			ps.setString(5, usr.getDateOfBirth());
+			ps.setString(6, usr.getUsername());
+			ps.setString(7, usr.getPassword());
+			ps.executeUpdate();
+			// close JDBC objects
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = "Something went wrong !!";
+		}
+		return result;
 	}
-	return result;
-}
-public User getStudents(String usrname, String password) throws Exception {
-	
-	User user = null;
-	
-	Connection myConn = null;
-	Statement myStmt = null;
-	ResultSet myRs = null;
-	
-	try {
-		// get a connection
-		myConn = dataSource.getConnection();
-		
-		// create sql statement
-		String sql = "SELECT dateOfBirth, firstName, lastName, mobile, email, userName, password FROM users where id = " + usrname;
-		
-		myStmt = myConn.createStatement();
-		
-		// execute query
-		myRs = myStmt.executeQuery(sql);
-		
-		// process result set
-		myRs.next();
-			
+
+	public User getUser(String usrname, String password) throws Exception {
+
+		User user = null;
+		Connection myconn = null;
+		Statement mystmt = null;
+		ResultSet myrs = null;
+
+		try {
+			// get a connection
+			myconn = dataSource.getConnection();
+
+			// create sql statement
+			String sql = "SELECT dateOfBirth, firstName, lastName, mobile, email, userName, password FROM users where id = "
+					+ usrname;
+
+			mystmt = myconn.createStatement();
+
+			// execute query
+			myrs = mystmt.executeQuery(sql);
+
+			// process result set
+			myrs.next();
+
 			// retrieve data from result set row
 
-			String fname = myRs.getString("firstName");
-			String lname = myRs.getString("lastName");
-			String dbirth = myRs.getString("dateOfBirth");
-			String email = myRs.getString("email");
-			String mobile = myRs.getString("mobile");
-			String uname = myRs.getString("dateOfBirth");
-			String pass = myRs.getString("password");
+			String fname = myrs.getString("firstName");
+			String lname = myrs.getString("lastName");
+			String dbirth = myrs.getString("dateOfBirth");
+			String email = myrs.getString("email");
+			String mobile = myrs.getString("mobile");
+			String uname = myrs.getString("dateOfBirth");
+			String pass = myrs.getString("password");
 
-			user = new User(fname, lname, dbirth, email, mobile, uname, pass);
-
-		
-		return user;		
-	}
-	finally {
-		// close JDBC objects
-		close(myConn, myStmt, myRs);
-	}		
-}
-
-private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
-
-	try {
-		if (myRs != null) {
-			myRs.close();
-		}
-		
-		if (myStmt != null) {
-			myStmt.close();
-		}
-		
-		if (myConn != null) {
-			myConn.close();   // doesn't really close it ... just puts back in connection pool
+			if (password == paa && username == uname && uname != null) {
+				user = new User(fname, lname, dbirth, email, mobile, uname, pass);
+			}
+			return user;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// close JDBC objects
+			close(myconn, mystmt, myrs);
 		}
 	}
-	catch (Exception exc) {
-		exc.printStackTrace();
+
+	private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+
+		try {
+			if (myRs != null) {
+				myRs.close();
+			}
+
+			if (myStmt != null) {
+				myStmt.close();
+			}
+
+			if (myConn != null) {
+				myConn.close();
+			}
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
-}
 }
